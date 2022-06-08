77befe700a897e910dfeb1c1a7868f48651d7cdb
Joao Vitor P <jvpereirafaustino@gmail.com>
2022-06-08 15:20:04 -0300

    Adicionado Camada Services e Repository

---
+++ app/Repositories/Eloquent/SaleRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Repositories\Contracts\SaleRepositoryInterface;
+
+class SaleRepository implements SaleRepositoryInterface
+{
+
+}
---
+++ app/Repositories/Eloquent/SellerRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Repositories\Contracts\SellerRepositoryInterface;
+
+class SellerRepository implements SellerRepositoryInterface
+{
+
+}
